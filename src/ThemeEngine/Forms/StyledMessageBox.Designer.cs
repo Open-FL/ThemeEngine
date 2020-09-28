0cf8d1406ba5ab9c673122383973d7e8c27cfffc
Tim Akermann <ti.akermann@gmail.com>
2020-09-28 20:46:46 +0200

    Added Custom Message Box with Theme Engine Support.

---
+++ src/ThemeEngine/Forms/StyledMessageBox.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ThemeEngine.Forms
+{
+    partial class StyledMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDialogButtons = new System.Windows.Forms.Panel();
+            this.flpDialogButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnRetry = new System.Windows.Forms.Button();
+            this.panelImage = new System.Windows.Forms.Panel();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.panelDialogContent = new System.Windows.Forms.Panel();
+            this.btnIgnore = new System.Windows.Forms.Button();
+            this.panelDialogButtons.SuspendLayout();
+            this.flpDialogButtons.SuspendLayout();
+            this.panelImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelDialogButtons
+            // 
+            this.panelDialogButtons.Controls.Add(this.flpDialogButtons);
+            this.panelDialogButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelDialogButtons.Location = new System.Drawing.Point(0, 217);
+            this.panelDialogButtons.Name = "panelDialogButtons";
+            this.panelDialogButtons.Size = new System.Drawing.Size(587, 38);
+            this.panelDialogButtons.TabIndex = 0;
+            // 
+            // flpDialogButtons
+            // 
+            this.flpDialogButtons.Controls.Add(this.btnAbort);
+            this.flpDialogButtons.Controls.Add(this.btnCancel);
+            this.flpDialogButtons.Controls.Add(this.btnOK);
+            this.flpDialogButtons.Controls.Add(this.btnNo);
+            this.flpDialogButtons.Controls.Add(this.btnYes);
+            this.flpDialogButtons.Controls.Add(this.btnRetry);
+            this.flpDialogButtons.Controls.Add(this.btnIgnore);
+            this.flpDialogButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpDialogButtons.Location = new System.Drawing.Point(0, 0);
+            this.flpDialogButtons.Name = "flpDialogButtons";
+            this.flpDialogButtons.Padding = new System.Windows.Forms.Padding(5);
+            this.flpDialogButtons.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.flpDialogButtons.Size = new System.Drawing.Size(587, 38);
+            this.flpDialogButtons.TabIndex = 0;
+            this.flpDialogButtons.WrapContents = false;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(337, 8);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(499, 8);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 23);
+            this.btnAbort.TabIndex = 1;
+            this.btnAbort.Text = "Abort";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(418, 8);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(175, 8);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 3;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(256, 8);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 4;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnRetry
+            // 
+            this.btnRetry.Location = new System.Drawing.Point(94, 8);
+            this.btnRetry.Name = "btnRetry";
+            this.btnRetry.Size = new System.Drawing.Size(75, 23);
+            this.btnRetry.TabIndex = 5;
+            this.btnRetry.Text = "Retry";
+            this.btnRetry.UseVisualStyleBackColor = true;
+            this.btnRetry.Click += new System.EventHandler(this.btnRetry_Click);
+            // 
+            // panelImage
+            // 
+            this.panelImage.Controls.Add(this.pbImage);
+            this.panelImage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelImage.Location = new System.Drawing.Point(0, 0);
+            this.panelImage.Name = "panelImage";
+            this.panelImage.Size = new System.Drawing.Size(117, 217);
+            this.panelImage.TabIndex = 1;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(9, 50);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(100, 100);
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // panelDialogContent
+            // 
+            this.panelDialogContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDialogContent.Location = new System.Drawing.Point(117, 0);
+            this.panelDialogContent.Name = "panelDialogContent";
+            this.panelDialogContent.Size = new System.Drawing.Size(470, 217);
+            this.panelDialogContent.TabIndex = 2;
+            // 
+            // btnIgnore
+            // 
+            this.btnIgnore.Location = new System.Drawing.Point(13, 8);
+            this.btnIgnore.Name = "btnIgnore";
+            this.btnIgnore.Size = new System.Drawing.Size(75, 23);
+            this.btnIgnore.TabIndex = 6;
+            this.btnIgnore.Text = "Ignore";
+            this.btnIgnore.UseVisualStyleBackColor = true;
+            this.btnIgnore.Click += new System.EventHandler(this.btnIgnore_Click);
+            // 
+            // StyledMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 255);
+            this.Controls.Add(this.panelDialogContent);
+            this.Controls.Add(this.panelImage);
+            this.Controls.Add(this.panelDialogButtons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StyledMessageBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StyledMessageBox";
+            this.panelDialogButtons.ResumeLayout(false);
+            this.flpDialogButtons.ResumeLayout(false);
+            this.panelImage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDialogButtons;
+        private System.Windows.Forms.FlowLayoutPanel flpDialogButtons;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnRetry;
+        private System.Windows.Forms.Panel panelImage;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Panel panelDialogContent;
+        private System.Windows.Forms.Button btnIgnore;
+    }
+}
---
+++ src/ThemeEngine/Forms/StyledMessageBox.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThemeEngine.Forms
+{
+    
+
+    public partial class StyledMessageBox : Form
+    {
+        [Flags]
+        public enum BorderButtons
+        {
+
+            Minimize = 1,
+            Maximize = 2,
+            Close = 4,
+
+        }
+
+        public enum MessageBoxContent
+        {
+
+            Text,
+            RichText,
+
+        }
+
+        private class StyledMessageBoxSettings
+        {
+
+            public string Title;
+            public string Content;
+            public Size Size;
+            public FormBorderStyle BorderStyle = FormBorderStyle.FixedDialog;
+            public BorderButtons ActivatedBorderButtons = BorderButtons.Close;
+            public MessageBoxButtons ActivatedDialogButtons = MessageBoxButtons.OK;
+            public Icon MessageBoxIcon;
+            public Image MessageBoxSideImage;
+            public MessageBoxContent ContentStyle = MessageBoxContent.Text;
+
+        }
+
+        private StyledMessageBox(StyledMessageBoxSettings settings)
+        {
+            InitializeComponent();
+
+            Text = settings.Title;
+            if (!settings.Size.IsEmpty)
+            {
+                Size = settings.Size;
+            }
+
+            Control contentContainer = CreateContentControl(settings.Content, settings.ContentStyle);
+
+            MinimizeBox = (settings.ActivatedBorderButtons & BorderButtons.Minimize) != 0;
+            MaximizeBox = (settings.ActivatedBorderButtons & BorderButtons.Maximize) != 0;
+
+            if ((settings.ActivatedBorderButtons & BorderButtons.Close) == 0)
+            {
+                //TODO
+            }
+
+            FormBorderStyle = settings.BorderStyle;
+
+            if (settings.MessageBoxIcon != null)
+            {
+                Icon = settings.MessageBoxIcon;
+            }
+
+            if (settings.MessageBoxSideImage != null)
+            {
+                pbImage.Image = settings.MessageBoxSideImage;
+            }
+            else
+            {
+                panelImage.Visible = false;
+            }
+
+            btnOK.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.OK ||
+                            settings.ActivatedDialogButtons == MessageBoxButtons.OKCancel;
+
+            btnYes.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.YesNo ||
+                             settings.ActivatedDialogButtons == MessageBoxButtons.YesNoCancel;
+
+            btnNo.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.YesNo ||
+                            settings.ActivatedDialogButtons == MessageBoxButtons.YesNoCancel;
+
+            btnAbort.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.AbortRetryIgnore;
+
+            btnCancel.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.RetryCancel ||
+                               settings.ActivatedDialogButtons == MessageBoxButtons.OKCancel ||
+                               settings.ActivatedDialogButtons == MessageBoxButtons.YesNoCancel;
+
+            btnRetry.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.RetryCancel ||
+                               settings.ActivatedDialogButtons == MessageBoxButtons.AbortRetryIgnore;
+            btnIgnore.Visible = settings.ActivatedDialogButtons == MessageBoxButtons.AbortRetryIgnore;
+
+            StyleManager.RegisterControls(this);
+        }
+
+        private Control CreateContentControl(string content, MessageBoxContent contentStyle)
+        {
+            if (contentStyle == MessageBoxContent.RichText)
+            {
+                RichTextBox tb = new RichTextBox();
+                tb.Parent = panelDialogContent;
+                tb.Dock = DockStyle.Fill;
+                tb.Text = content;
+                return tb;
+            }
+            else
+            {
+                Label lbl = new Label();
+                lbl.Parent = panelDialogContent;
+                lbl.Dock = DockStyle.Fill;
+                lbl.AutoSize = true;
+                lbl.Padding = new Padding(5);
+                lbl.Text = content;
+                return lbl;
+            }
+        }
+
+        private void FinalizeDialog(DialogResult result)
+        {
+            DialogResult = result;
+            Close();
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.Abort);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.Cancel);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.OK);
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.No);
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.Yes);
+        }
+
+        private void btnRetry_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.Retry);
+        }
+
+        private void btnIgnore_Click(object sender, EventArgs e)
+        {
+            FinalizeDialog(DialogResult.Ignore);
+        }
+
+        public static void Show(
+            string title, string content, MessageBoxContent contentStyle = MessageBoxContent.Text)
+        {
+            StyledMessageBoxSettings settings = new StyledMessageBoxSettings
+            {
+                Title = title,
+                ContentStyle = contentStyle,
+                Content = content
+            };
+            StyledMessageBox mbox = new StyledMessageBox(settings);
+            mbox.ShowDialog();
+        }
+
+        public static DialogResult Show(
+            string title, string content, Size size, MessageBoxButtons buttons,
+            MessageBoxContent contentStyle = MessageBoxContent.Text,
+            FormBorderStyle borderStyle = FormBorderStyle.FixedDialog,
+            BorderButtons borderButtons = BorderButtons.Close, Icon messageBoxIcon = null,
+            Image messageBoxImage = null)
+        {
+            StyledMessageBoxSettings settings = new StyledMessageBoxSettings
+            {
+                Title = title,
+                ContentStyle = contentStyle,
+                Content = content,
+                Size = size,
+                ActivatedDialogButtons = buttons,
+                ActivatedBorderButtons = borderButtons,
+                BorderStyle = borderStyle,
+                MessageBoxSideImage = messageBoxImage,
+                MessageBoxIcon = messageBoxIcon
+            };
+
+            StyledMessageBox mbox = new StyledMessageBox(settings);
+            return mbox.ShowDialog();
+        }
+
+    }
+}
